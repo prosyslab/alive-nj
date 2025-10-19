a1aa7f254c2a9d56a10a1a469500953cf600a904
Bongjun <bongjun.jang@kaist.ac.kr>
2025-10-19 11:08:40 +0000

    [Script] update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env bash
 
+sudo apt install -y build-essential checkinstall libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev
+wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
+tar xzf Python-2.7.18.tgz
+cd Python-2.7.18
+./configure --enable-optimizations
+make
+sudo make install
+
+python2.7 -V | grep "2.7.18"
+
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 sudo python2.7 get-pip.py
 python2.7 -m pip install z3-solver==4.5.1.0
